2006dfd59903fe0511850a115f36f79af1fd1290
WilliamJSS <williamsena088@gmail.com>
2022-11-05 15:45:08 -0300

    feat: Added todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import { useState } from "react";
+import { MdDelete } from 'react-icons/md'
 import "./App.css";
 
 function App() {
   const KEY_ENTER = 13;
   const KEY_ESCAPE = 27;
 
+  const initialTodos = [
+    { id: 1, title: 'Estudar React', checked: false },
+    { id: 2, title: 'Estudar Inglês', checked: true },
+    { id: 3, title: 'Tocar guitarra', checked: false },
+  ]
+
+  const [todos, setTodos] = useState(initialTodos);
   const [value, setValue] = useState('');
 
   const clear = () => setValue('');
@@ -38,7 +46,16 @@ function App() {
           value={value}
           onChange={onChange}
           onKeyDown={onKeyDown} />
-        <p>My task: {value}</p>
+        <ul className="todo-list">
+          {todos.map((todo) => (
+            <li key={todo.id.toString()}>
+              <span className="todo">{todo.title}</span>
+              <button className="remove">
+                <MdDelete size={28}></MdDelete>
+              </button>
+            </li>
+          ))}
+        </ul>
       </section>
     </section>
   );
